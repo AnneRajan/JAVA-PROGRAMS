f1f6b8725cccca03d9e67c8181fb73d288a26b60
Anne Rajan <50075447+AnneRajan@users.noreply.github.com>
2019-07-21 01:29:38 +0530

    Create evenorodd.java
    even or odd number for 1-10
    
    output
    Number is a odd number  1
    Number is a even number  2
    Number is a odd number  3
    Number is a even number  4
    Number is a odd number  5
    Number is a even number  6
    Number is a odd number  7
    Number is a even number  8
    Number is a odd number  9
    Number is a even number  10

---
+++ evenorodd.java
@@ -0,0 +1,22 @@
+ //Even or ODD
+    public static void main(String[] args)
+    {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        
+        for(int i=1;i<=10;i++)
+        {
+            if(i%2==0)
+            {
+                System.out.println("Number is a even number  " +i);
+            }
+            else
+            {
+                System.out.println("Number is a odd number  " +i);
+            }
+        }
+        
+    }
+    
+}
