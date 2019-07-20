ff8b6af9d4a63c3622ba519ce9875fd3be5efb96
Anne Rajan <50075447+AnneRajan@users.noreply.github.com>
2019-07-21 03:04:04 +0530

    Create fibo.java

---
+++ fibo.java
@@ -0,0 +1,16 @@
+//FIBONACCI
+public static void main(String args[])
+{
+    int a=0,b = 1,c,i;
+    for(i=1;i<=8;i++)
+    {
+        System.out.println("" +a);
+        c=a+b;
+        
+        a=b;
+        b=c;
+        
+    }
+}
+
+}
