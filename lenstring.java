781a55b0f2123c8e1dc6a25bb284981bde2493ee
Anne Rajan <50075447+AnneRajan@users.noreply.github.com>
2019-08-02 12:49:13 +0530

    Create lenstring.java

---
+++ lenstring.java
@@ -0,0 +1,14 @@
+public static void main(String args[])
+    {
+        String s1,s2;
+        Scanner sc = new Scanner(System.in);
+        System .out.println("Enter 2 strings:");
+        s1=sc.nextLine();
+        s2=sc.nextLine();
+        
+        int len1,len2;
+        len1=s1.length();
+        System.out.println("Length of String1: "+len1);
+        len2=s2.length();
+        System.out.println("Length of String1: "+len2);
+    }
