d189d1f3f4fb097d977495ed122b3999d195d1b1
Anne Rajan <50075447+AnneRajan@users.noreply.github.com>
2019-08-02 16:19:19 +0530

    Create uppercase.java

---
+++ uppercase.java
@@ -0,0 +1,9 @@
+public static void main(String args[])
+    {
+        String s1;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a word:");
+        s1=sc.nextLine();
+        s1=s1.toUpperCase();
+        System.out.println("Upper case word: "+s1);
+    } 
